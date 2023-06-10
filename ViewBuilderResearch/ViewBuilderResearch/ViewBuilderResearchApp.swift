2b3f994996241db4cc4f8c80b656f630399961b6
Kyeoeol <safari-earshot0a@icloud.com>
2023-06-10 15:12:43 +0900

    init commit

---
+++ ViewBuilderResearch/ViewBuilderResearch/ViewBuilderResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuilderResearchApp.swift
+//  ViewBuilderResearch
+//
+//  Created by @Kyeol on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuilderResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
