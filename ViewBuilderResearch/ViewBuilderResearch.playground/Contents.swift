c355be12785979d087dd67ee51c4ec0798cc8a2f
Kyeoeol <safari-earshot0a@icloud.com>
2023-06-18 11:44:04 +0900

    Add more basic views

--- ViewBuilderResearch/ViewBuilderResearch.playground/Contents.swift
+++ ViewBuilderResearch/ViewBuilderResearch.playground/Contents.swift
@@ -6,19 +6,41 @@ func makeMyView() -> some ImitatingView {}
 print("type of:", type(of: makeMyView()))
 // Print: type of: ImitatingEmptyView
 
-//struct MyContentView: ImitatingView {
-//    var body: some ImitatingView {
-//        ImitatingEmptyView()
-//    }
-//}
-//MyContentView().body.debug()
+struct MyContentView1: ImitatingView {
+    var body: some ImitatingView {
+        ImitatingEmptyView()
+    }
+}
+MyContentView1().body.debug()
 // Print: ImitatingEmptyView
 
-struct MyContentView: ImitatingView {
+struct MyContentView2: ImitatingView {
     var body: some ImitatingView {
         ImitatingEmptyView()
         ImitatingEmptyView()
     }
 }
-MyContentView().body.debug()
+MyContentView2().body.debug()
 // Print: TupleView<(ImitatingEmptyView, ImitatingEmptyView)>
+
+struct MyContentView3: ImitatingView {
+    var body: some ImitatingView {
+        ImitatingGroup {
+            ImitatingEmptyView()
+            ImitatingEmptyView()
+        }
+    }
+}
+MyContentView3().body.debug()
+// Print: ImitatingGroup<TupleView<(ImitatingEmptyView, ImitatingEmptyView)>>
+
+struct MyContentView4: ImitatingView {
+    var body: some ImitatingView {
+        ImitatingGroup {
+            ImitatingEmptyView()
+            ImitatingText("Hello, Playground!")
+        }
+    }
+}
+MyContentView4().body.debug()
+// Print: ImitatingGroup<TupleView<(ImitatingEmptyView, ImitatingText)>>
--- ViewBuilderResearch/ViewBuilderResearch/ImitatingView.swift
+++ ViewBuilderResearch/ViewBuilderResearch/ImitatingView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+
+// MARK: ImitatingView
+
 public protocol ImitatingView {
     associatedtype Body: ImitatingView
     @ImitatingViewBuilder var body: Self.Body { get }
@@ -26,15 +29,39 @@ public extension ImitatingView {
 /// Therefore, we need to make Never conform to the View protocol to continue the work below:
 extension Never: ImitatingView {
     public typealias Body = Never
-    public var body: Never { return fatalError() }
+    public var body: Never { fatalError() }
 }
 
 
+// MARK: Basic Views
+
+// EmptyView
 /// EmptyView is a view that does nothing, it is just an empty view
 public struct ImitatingEmptyView: ImitatingView {
     public typealias Body = Never
-    public var body: Never { return fatalError() }
+    public var body: Never { fatalError() }
     public init() {}
 }
 
 
+// Group
+public struct ImitatingGroup<Content>: ImitatingView {
+    var content: Content
+    public var body: Never { fatalError() }
+    public init(@ImitatingViewBuilder _ content: () -> Content) {
+        self.content = content()
+    }
+}
+
+
+// Text
+public struct ImitatingText: ImitatingView {
+    public typealias Body = Never
+    public var body: Never { fatalError() }
+    /// In SwiftUI, an enumeration type is used to distinguish between String and LocalizedStringKey,
+    /// and the imitation process will be simplified uniformly.
+    var content: String
+    public init(_ content: String) {
+        self.content = content
+    }
+}
